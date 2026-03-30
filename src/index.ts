7297557851f750468f21d33e88c76af23b628e5b
Rahul Gangwar <rahulgangwar4567@gmail.com>
2026-03-30 16:10:17 +0530

    added

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors({
-  origin: ['https://sol-pay.netlify.app', 'http://localhost:5173'],
+  origin: ['https://sol-pay.netlify.app', 'http://localhost:5173', 'http://localhost:8080'],
   credentials: true
 }));
 app.use(helmet({
